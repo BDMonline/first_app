6838265e8d2d2702e2c3e585068d4af5c8d43788
Brian Medley <brian.spam@ntlworld.com>
2012-06-04 08:36:08 +0100

    add users and password system

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
+class User < ActiveRecord::Base
+  attr_accessible :email, :name , :password,:password_confirmation  
+         #ensures only these columns are acc'ble - don't worry, the p'word ones don't persist.
+  has_secure_password
+  
+  before_save { |user| user.email = email.downcase } #always save email in lowercase
+  
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+			uniqueness:  { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+end
