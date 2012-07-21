f3aeb09de3f3868c67cda5bceab46ff62b934bdd
Brian Medley <brian.spam@ntlworld.com>
2012-07-21 19:20:29 +0100

    .

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,9 +9,10 @@
 //
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
+//= require bootstrap
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
 //= require prettyphoto-rails
-//= require bootstrap
+
 
