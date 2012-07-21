1cc9d87d5cf389c11fa8f38d9c4fb8bff923cf16
Brian Medley <brian.spam@ntlworld.com>
2012-07-22 00:29:39 +0100

    fixed bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -569,8 +569,8 @@ module ApplicationHelper
             answer="0"
             if figs>1
                 answer=answer+'.'+'0'*(figs-1)
-                return answer
             end
+            return answer
         end
         number = number.abs
         exponent=(log10(number)).floor
@@ -808,7 +808,7 @@ module ApplicationHelper
                         # <h2> <iframe frameborder="0" width="480" height="360" src= )+element.content+%Q( > </iframe><br /></i> </h2>
                         #                     )
                     end
-                    @item_html=@item_html+'<td style="vertical-align:middle" > ' +content_html+' </td> <td style="vertical-align:middle"> <p align="right">'+hint_html+%Q(</p> </h9> 
+                    @item_html=@item_html+'<td style="vertical-align:middle" > ' +content_html+' </td> <td style="vertical-align:middle"> <p align="right">'+hint_html+%Q(</p> 
                     </td> 
                     </tr>
                     )
