8faa3f558655b4877442abb6e28b3638db369d1f
Brian Medley <brian.spam@ntlworld.com>
2012-07-22 13:28:30 +0100

    improved elements index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,15 @@ module ApplicationHelper
 
 	include Math
 
+    def sortable(column, title = nil)
+        title ||= column.titleize
+        css_class = (column == sort_column) ? "current #{sort_direction}" : nil
+        direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
+        link_to title, params.merge(:sort => column, :direction => direction, :page => nil), {:class => css_class}
+    end
+
+
+
 	def ln(x)
 	    return log(x)
 	end
