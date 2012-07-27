0a90f4f65a5878a531713d125faaa11bf004bfea
Brian Medley <brian.spam@ntlworld.com>
2012-07-27 23:21:07 +0100

    securing secret key

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = 'ca546f9b21141ec010e701c153dc1a5f6e4a49cc0a9e26ab2f6d915c979c29ba568659c404d2d60ee8f7384e9c9377b441c7d012d78e925f564a0148d3824731'
+FirstApp::Application.config.secret_token = ENV[SECRET_KEY]
