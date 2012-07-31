fd56263fb5e619f4f87d3bec4cb328756f2cbb7d
Brian Medley <brian.spam@ntlworld.com>
2012-07-31 22:56:20 +0100

    changed title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -149,7 +149,7 @@ module ApplicationHelper
 
 	# Returns the full title on a per page basis
 	def full_title(page_title)
-		base_title="RoboTutor"
+		base_title="StemLoops"
 		if page_title.empty?
 			base_title
 		else
