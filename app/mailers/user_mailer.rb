413f4d7c3eaf397aabcf7ff24cc438a30516d8ff
Brian Medley <brian.spam@ntlworld.com>
2012-07-31 23:35:45 +0100

    updated production env domain name

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ActionMailer::Base
   default from: "donotreply@stemloops.co.uk"
   def registration_confirmation(user)
   	@user = user
-  	mail(:to => user.email, :subject => "Registered", :from => "zero.xwords@ntlworld.com")
+  	mail(:to => user.email, :subject => "Registered")
   end
 
   def password_reset(user)
