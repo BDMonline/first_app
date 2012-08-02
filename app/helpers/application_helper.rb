bdf3dbc26726f35a7f234a21c947863153d17970
Brian Medley <brian.spam@ntlworld.com>
2012-08-02 22:41:44 +0100

    fixed bug with item view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -857,7 +857,8 @@ def users_browser_ie?
         <td>
         )
 
-        @item_html=@item_html+%Q(
+        @item_html=@item_html+%Q(<input id="really" name="really" type="hidden" value="yes" />
+        <input id="profile_id" name="profile_id" type="hidden" value=")+params[:profile_id]+ %Q(" />
         <input type="image" value="Check answers" src="http://i970.photobucket.com/albums/ae189/gumboil/Checkbutton.png" alt="Check Answers" width="150">
         </form>
         )
